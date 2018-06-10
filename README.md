abfac24104f1fd6cc4f2cfe0682e0f8ac483e8c0
ByskyXie <547404396@qq.com>
2018-06-10 11:06:19 +0800

    database plus.

--- README.md
+++
@@ -1,9 +0,0 @@
-# Eluyouni Database
-
-## 简介
-
-“医路有我”APP数据库
-
-## LICENSE
-
-[GPL-2.0](http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html) or later.
--- article_patient.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `article_patient` VALUES (10001, 15212345678, '假装是标题', '2018-5-11 11:19:48', '假装是正文');
-INSERT INTO `article_patient` VALUES (10002, 15207910001, '假装是文章', '2018-5-11 14:29:14', 'null');
-INSERT INTO `article_patient` VALUES (10003, 15207910002, '第三篇文章', '2018-5-11 14:30:05', 'null');
